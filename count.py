b907427e1a48cca2017959905c8f56d684d952af
syfulla <82517195+syfulla@users.noreply.github.com>
2021-06-20 06:37:06 +0530

    Create count.py
    In itertools the count is a simple and basic function which counts the given list or we can count without list 
    Basically it starts with zero, then it goes on increment by one 
    next( ) is a built in function which stores the current value and gives the next value this is what happens when we use looping

---
+++ count.py
@@ -0,0 +1,13 @@
+import itertools
+counter=itertools.count()
+print(next(counter)) # next() is a built in function which gives the next value from the begining
+print(next(counter))
+print(next(counter))
+print(next(counter))
+
+
+
+output:0
+  1
+  2
+  3
